142568a090a8c3c605972123de9d054e86cb762d
TanishqKBhardwaj <kvbharwaj4@email.com>
2025-03-12 00:19:36 +0530

    Fixing bug

--- app/(main)/interview/page.tsx
+++ app/(main)/interview/page.tsx
@@ -7,6 +7,7 @@ import QuizStats from './components/QuizStats'
 import QuizGraph from './components/QuizGraph'
 import QuizList from './components/QuizList'
 import {getAssessments} from '../../../actions/interview'
+import Link from 'next/link';
 
 async function page() {
     const {isOnBoarded}= await getUserOnboardingStatus();
@@ -25,6 +26,13 @@ async function page() {
         <h1 className='text-center text-3xl md:text-5xl font-bold'>
           Interview <span className='text-purple-500'>Preparation</span>
         </h1>
+       <div className='flex place-content-center'>
+        <button className=" mx-auto relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    <Link href='/interview/mock'>Start Quiz</Link>
+  </span>
+</button></div>
 
  
 
